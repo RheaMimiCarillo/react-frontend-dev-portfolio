9748ecadcecf28927769435df7380a5d45bd97d8
RheaMimiCarillo <RheaMimiCarillo@gmail.com>
2024-12-03 14:43:16 +0000

    Updates

---
+++ precache-manifest.68fdbfdcf48551f04af559e54de5fdf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b47c8c4ee8a2e59b70da5ae4433613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c0a60fd6d42f18338c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "77aa0f7fe64e05ffcf48",
+    "url": "/static/css/main.154e5e74.chunk.css"
+  },
+  {
+    "revision": "21c0a60fd6d42f18338c",
+    "url": "/static/js/2.2654e2bc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.2654e2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aa0f7fe64e05ffcf48",
+    "url": "/static/js/main.85c9839d.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
